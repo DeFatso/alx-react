bfa1c6b06d4fae5bedf5e91933d010815cec0379
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-06-13 22:18:47 +0200

    8-seq.js

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,18 +1,24 @@
-const { Seq } = require('immutable');
+import { Seq } from 'immutable';
 
-export function printBestStudents(grades) {
-    const result = Seq(grades)
-        .filter(student => student.score >= 70)
-        .map(student => ({
-            score: student.score,
-            firstName: capitalize(student.firstName),
-            lastName: capitalize(student.lastName),
-        }))
-        .toObject();
+export default function printBestStudents(object) {
+  const seq = Seq(object);
 
-    console.log(result);
-}
+  const filtered = seq.filter((student) => {
+    student.firstName.charAt(0).toUpperCase();
+    return student.score > 70;
+  });
 
-export function capitalize(name) {
-    return name.charAt(0).toUpperCase() + name.slice(1);
+  function capFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const JSObject = filtered.toJS();
+
+  Object.keys(JSObject).map((key) => {
+    JSObject[key].firstName = capFirstLetter(JSObject[key].firstName);
+    JSObject[key].lastName = capFirstLetter(JSObject[key].lastName);
+    return JSObject[key];
+  });
+
+  console.log(JSObject);
 }
