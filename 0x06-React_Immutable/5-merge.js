29c823fb98cdefc960f3b779df1850e983af2c86
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-06-14 02:54:19 +0200

    5-merge.js

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,8 +1,6 @@
-// Import Immutable.js
 import { List, Map } from 'immutable';
 
-// Function to concatenate two arrays into a List
-function concatElements(page1, page2) {
+export default function concatElements(page1, page2) {
     // Convert arrays to Immutable Lists and concatenate them
     return List(page1).concat(List(page2));
 }
@@ -19,13 +17,3 @@ function mergeElements(page1, page2) {
     // Return the values of the merged map as a List
     return mergedMap.toList();
 }
-
-// Example usage:
-const page1Array = [1, 2, 3];
-const page2Array = [4, 5, 6];
-
-const page1Object = { a: 1, b: 2, c: 3 };
-const page2Object = { b: 20, d: 4 };
-
-console.log(concatElements(page1Array, page2Array)); // List [ 1, 2, 3, 4, 5, 6 ]
-console.log(mergeElements(page1Object, page2Object)); // List [ 1, 20, 3, 4 ]
